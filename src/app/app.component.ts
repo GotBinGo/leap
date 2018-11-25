6d5021ba7649b8c2286b3890c8ab4b7778a9c878
Tamás Bordák <bordak.tamas@autsoft.hu>
2018-11-25 02:04:33 +0100

    chat name display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  messages = [{message: 'a', self: true}, {message: 'b', self: false}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}];
+  messages = [{message: 'a', self: true}, {message: 'b', sender: 'Toomi', self: false}, {message: 'c', self: false, sender: 'Toomi'}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}];
   chatInput = '';
   send = () => {
-    this.messages.push({message: this.chatInput, self: this.chatInput.length%2==0});
+    this.messages.push({message: this.chatInput, self: this.chatInput.length % 2 === 0, sender: 'Toomi'});
     this.chatInput = '';
     console.log(scroll);
     setTimeout(() => {document.getElementById('scroll').scrollTo(0, 999999999); }, 100);
