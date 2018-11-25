d3c7c0ae244c6e55abda6544cd4b4849bb302a62
Tamás Bordák <bordak.tamas@autsoft.hu>
2018-11-25 01:07:54 +0100

    chat scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  messages = [{message: 'a', self: true}, {message: 'b', self: false}, {message: 'c', self: true}];
+  messages = [{message: 'a', self: true}, {message: 'b', self: false}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}, {message: 'c', self: true}];
+  chatInput = '';
+  send = () => {
+    this.messages.push({message: this.chatInput, self: this.chatInput.length%2==0});
+    this.chatInput = '';
+    console.log(scroll);
+    setTimeout(() => {document.getElementById('scroll').scrollTo(0, 999999999); }, 100);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule, MatCardModule, MatSidenavModule, MatInputModule, MatIconModule, MatChipsModule } from '@angular/material';
 
 @NgModule({
@@ -10,6 +11,7 @@ import { MatButtonModule, MatCardModule, MatSidenavModule, MatInputModule, MatIc
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
