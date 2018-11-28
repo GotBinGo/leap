f3075c40bc919b7b27baa38f1e4e6600a4391a9f
Tamás Bordák <bordak.tamas@autsoft.hu>
2018-11-28 20:50:11 +0100

    game in angular

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, HostListener } from '@angular/core';
+import * as THREE from 'three';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -9,8 +9,138 @@ export class GameComponent implements OnInit {
 
   constructor() { }
 
+  renderer = new THREE.WebGLRenderer();
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  controls = {enabled: false};
+  pitchObject = new THREE.Object3D();
+  yawObject = new THREE.Object3D();
+  instructions = document.getElementById( 'instructions' );
+
   ngOnInit() {
-    
+    this.renderer.shadowMapEnabled = true;
+    // this.renderer.shadowMapSoft = true;
+    // renderer.shadowMap.enabled = true;
+    // renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    const cand = document.getElementById( 'cand' );
+    cand.appendChild( this.renderer.domElement );
+
+
+    const ambient = new THREE.AmbientLight( 0x666666 );
+    this.scene.add( ambient );
+
+    const geometry = new THREE.PlaneGeometry( 300, 300, 500, 500);
+    geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+
+    const material = new THREE.MeshLambertMaterial( { color: 0xdddddd } );
+
+    const mesh = new THREE.Mesh( geometry, material );
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    this.scene.add( mesh );
+
+    this.pitchObject = new THREE.Object3D();
+    this.pitchObject.add( this.camera );
+
+    this.yawObject = new THREE.Object3D();
+    this.yawObject.position.y = 2;
+    this.yawObject.add( this.pitchObject );
+    this.scene.add( this.yawObject );
+
+    const light = new THREE.PointLight( 0xff0000, 1, 100 );
+    light.position.set(-20, 1, -10);
+    this.scene.add(light);
+    this.animate();
   }
 
+  animate = () => {
+    requestAnimationFrame(this.animate);
+    this.renderer.render( this.scene, this.camera );
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove = (event) => {
+    if ( this.controls.enabled === false ) {
+      return;
+    }
+
+    const movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+    const movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+
+    this.yawObject.rotation.y -= movementX * 0.002;
+    this.pitchObject.rotation.x -= movementY * 0.002;
+    const PI_2 = Math.PI / 2;
+    this.pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, this.pitchObject.rotation.x ) );
+  }
+
+  @HostListener('document:pointerlockchange', ['$event'])
+  @HostListener('document:mozpointerlockchange', ['$event'])
+  @HostListener('document:webkitpointerlockchange', ['$event'])
+  pointerLockChange = (event) => {
+    const blocker = document.getElementById( 'blocker' );
+    const instructions = document.getElementById( 'instructions' );
+    const element = document.body;
+    if (document['pointerLockElement'] === element ||
+      document['mozPointerLockElement'] === element ||
+      document['webkitPointerLockElement'] === element ) {
+      this.controls.enabled = true;
+
+        blocker.style.display = 'none';
+    } else {
+        this.controls.enabled = false;
+        blocker.style.display = '-webkit-box';
+        blocker.style.display = '-moz-box';
+        blocker.style.display = 'box';
+
+        instructions.style.display = '';
+    }
+  }
+
+  @HostListener('document:pointerlockerror', ['$event'])
+  @HostListener('document:mozpointerlockerror', ['$event'])
+  @HostListener('document:webkitpointerlockerror', ['$event'])
+  pointerLockError = ( event ) => {
+    const instructions = document.getElementById( 'instructions' );
+    instructions.style.display = '';
+  }
+
+  onClick = (event) => {
+    const instructions = document.getElementById( 'instructions' );
+    instructions.style.display = 'none';
+    const element: any = document.body;
+
+    element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+
+    if ( /Firefox/i.test( navigator.userAgent ) ) {
+        const fullscreenchange = function () {
+          if (document['fullscreenElement'] === element ||
+              document['mozFullscreenElement'] === element ||
+              document['mozFullScreenElement'] === element ) {
+
+              document.removeEventListener( 'fullscreenchange', fullscreenchange );
+              document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+              element.requestPointerLock();
+          }
+        };
+
+        document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+        document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+
+        element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen ||
+        element.mozRequestFullScreen || element.webkitRequestFullscreen;
+
+        element.requestFullscreen();
+
+    } else {
+        element.requestPointerLock();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize = () => {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+  }
 }
